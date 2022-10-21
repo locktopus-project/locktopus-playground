310350eb6f3c06f19afd00c2b129b470ad34c8fe
xshkut <shkut.ap@gmail.com>
2022-10-21 15:53:20 +0000

    write some usage guide in Help

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -2,6 +2,10 @@ import {
   Button,
   Code,
   Flex,
+  Heading,
+  List,
+  ListIcon,
+  ListItem,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -9,12 +13,23 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  OrderedList,
   Text,
   useClipboard,
   useDisclosure,
 } from "@chakra-ui/react";
 import { LOCKTOPUS_SERVER_IMAGE_NAME } from "../constants";
-import { CopyIcon, CheckIcon } from "@chakra-ui/icons";
+import {
+  CopyIcon,
+  CheckIcon,
+  ArrowRightIcon,
+  LockIcon,
+  UnlockIcon,
+  CloseIcon,
+  SmallCloseIcon,
+  PlusSquareIcon,
+  AddIcon,
+} from "@chakra-ui/icons";
 
 export const Help = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -31,6 +46,9 @@ export const Help = () => {
           <ModalHeader>Welcome to Locktopus Playground</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
+            <Heading pb={4} size="lg">
+              How to setup
+            </Heading>
             <Text>
               Run local instance of Locktopus Server using ths command:
             </Text>
@@ -55,7 +73,41 @@ export const Help = () => {
                 <CopyButton text={connString} />
               </Flex>
             </CodeBlock>
-            <Text>After that, add locks with resources and try them out</Text>
+            <Text>Ensure the server is reachable (status ONLINE)</Text>
+            <Heading pb={4} size="lg">
+              How to use
+            </Heading>
+            <Text pb={2}>
+              Create multiple connections to test how locked resources interfere
+              with each other. For each connection, follow these steps:
+            </Text>
+            <List>
+              <ListItem>
+                <ListIcon as={ArrowRightIcon} color="green.500" />
+                Connect: establish connection to the server
+              </ListItem>
+              <ListItem>
+                <ListIcon as={AddIcon} />
+                Add Lock Resources. By default, you have only one resource with
+                a Path having no segments (pointing to root). Actually, that
+                means you will lock the entire namespace. Add segments to
+                specify concrete path you want to lock. Also, switch lock type
+                to READ or WRITE.
+              </ListItem>
+              <ListItem>
+                <ListIcon as={LockIcon} />
+                Lock: Try locking the specified set of resources. Wait until the
+                lock is acruired.
+              </ListItem>
+              <ListItem>
+                <ListIcon as={UnlockIcon} />
+                Release: Allow others to acquire the lock
+              </ListItem>
+              <ListItem>
+                <ListIcon as={SmallCloseIcon} color="yellow.500" />
+                Disconnect when you are done (though isn't necessary)
+              </ListItem>
+            </List>
           </ModalBody>
           <ModalFooter />
         </ModalContent>
@@ -66,7 +118,7 @@ export const Help = () => {
 
 const CodeBlock = (props: { children: any }) => {
   return (
-    <Code padding="5" rounded="8px" my="8" w="full">
+    <Code padding={4} rounded="8px" my="4" w="full">
       {props.children}
     </Code>
   );
