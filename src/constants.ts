cd051ba8586e168745f69b90f17b9048df723f20
xshkut <shkut.ap@gmail.com>
2022-10-21 14:55:48 +0000

    use empty token as default path segment

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const LOCKTOPUS_SERVER_IMAGE_NAME = "locktopus/locktopus";
-export const DEFAULT_SEGMENT_INPUT = "*";
+export const DEFAULT_SEGMENT_INPUT = "";
 export const DEFAULT_RESOURCE_WRITE = true;
